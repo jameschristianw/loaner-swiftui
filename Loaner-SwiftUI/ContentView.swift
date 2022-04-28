1b0ba07975135798533e5ae08a6e42f1f9b7e76a
James Christian Wira <jamesch888@gmail.com>
2022-04-29 00:02:26 +0700

    Initial Commit

---
+++ Loaner-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loaner-SwiftUI
+//
+//  Created by James Christian Wira on 29/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Loaner-SwiftUI/Loaner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loaner_SwiftUIApp.swift
+//  Loaner-SwiftUI
+//
+//  Created by James Christian Wira on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Loaner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
